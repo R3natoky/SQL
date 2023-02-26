10b4fd6a19c7759958c7015adb21ba7c3e6ee97b
Rene Lanza Lenovo <rlanzach@gmail.com>
2023-02-26 13:09:26 -0500

    Revision presentacion

--- SQLQuery CONSULTAS.sql
+++ SQLQuery CONSULTAS.sql
@@ -66,18 +66,24 @@ GO
 ---====================================================================
 
 SELECT 
-    EQP.Familia         AS EQUIPO,    
+    PAR.Descripcion_Partida AS PARTIDA,
+	EQP.Familia         AS EQUIPO,    
     EQP.Registro_Placa  AS REGISTRO,
     SUM(DET.Cantidad)   AS CANTIDAD,
-    DET.Und             AS UNIDAD
+    DET.Und             AS UNIDAD,
+	PARTE.Costo			AS PU,
+	SUM(DET.Cantidad*PARTE.Costo) AS COSTO
 
 FROM EQUIPO AS EQP
     INNER JOIN PARTE_EQUIPOS AS PARTE ON EQP.Id_Equipo=PARTE.Id_Equipo
     INNER JOIN Detalle_Parte AS DET   ON PARTE.Id_Parte=DET.Id_Parte
+	INNER JOIN PARTIDA		 AS PAR   ON PARTE.Id_Partida=PAR.Id_Partida
 GROUP BY 
-    EQP.Familia,
+    PAR.Descripcion_Partida,
+	EQP.Familia,
     EQP.Registro_Placa,
-    DET.Und;
+    DET.Und,
+	PARTE.Costo
 GO
 
 ---====================================================================
@@ -85,4 +91,59 @@ GO
 ---====================================================================
 
 DELETE FROM NOTA_ALMACEN;
+GO
 
+---====================================================================
+--- CONSULTAR MATERIALES UTILIZADOS POR PARTIDA
+---====================================================================
+
+SELECT
+	NOTA.Id_Partida,
+	PAR.Descripcion_Partida,
+	ALM.Id_Material,
+	MAT.Descripcion,
+	MAT.PU,
+	SUM(ALM.Cantidad)			AS CANTIDAD,
+	SUM(MAT.PU*ALM.Cantidad)	AS COSTO
+
+FROM NOTA_ALMACEN AS NOTA
+	INNER JOIN Detalle_Almacen	AS ALM ON NOTA.Id_Nota=ALM.Id_Nota
+	INNER JOIN MATERIALES		AS MAT ON ALM.Id_Material=MAT.Id_Material
+	INNER JOIN PARTIDA			AS PAR ON PAR.Id_Partida=NOTA.Id_Partida
+GROUP BY
+	ALM.Id_Material,
+	MAT.Descripcion,
+	NOTA.Id_Partida,
+	PAR.Descripcion_Partida,
+	MAT.PU;
+GO
+
+---====================================================================
+--- CONSULTAR PERSONAL POR PARTIDA
+--- NOTA: Corregir bug en calculo de HH, se tuvo que poner "-"
+---====================================================================
+
+SELECT
+	PAR.Descripcion_Partida		AS PARTIDA,
+	MO.Nro_Docummento			AS PLANILLA,
+	--TAR.Id_Personal				AS CODIGO,
+	PER.Categoria				AS Categoria,
+	SUM(-TAR.HH)				AS HORAS,
+	ROUND(PER.Salario/30/8,2)			AS [COSTO HORA],
+	ROUND(SUM(-TAR.HH*PER.Salario/30/8),2)		AS COSTO
+
+FROM TAREO_MO AS MO
+	INNER JOIN PARTIDA	AS PAR ON MO.Id_Partida=PAR.Id_Partida
+	INNER JOIN Detalle_Tareo AS TAR ON TAR.Id_Tareo=MO.Id_Tareo
+	INNER JOIN	PERSONAL	 AS PER ON PER.Id_Personal=TAR.Id_Personal
+
+GROUP BY
+	PER.Categoria				,
+	PAR.Descripcion_Partida		,
+	MO.Nro_Docummento			,
+	--TAR.Id_Personal				,
+	PER.Salario	
+ORDER BY 
+	PAR.Descripcion_Partida
+
+GO
